da299b074399d246306769295c3bbd0b8c8098dd
OyindamolaWbCode <kushimo.oyindamola@gmail.com>
2023-01-06 23:42:13 +0100

    profile display

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,11 +1,13 @@
 <script>
 import NavbarVue from "../components/Navbar.vue";
+import Image from "../assets/me.jpg";
 export default {
   name: "app",
   components: { NavbarVue },
   data() {
     return {
-      profile: null
+      profile: null,
+      Image: Image,
     }
   },
   methods: {
@@ -25,29 +27,59 @@ export default {
 </script>
 
 <template>
-  <main>
-    <div class="profileData">
-      <!-- <div v-for="profile in profiles" v-bind:key="profile.id">
-        <h6>{{profile.name}}</h6>
-        <p>{{profile.login}}</p>
-        <router-link>{{profile.html_url}}</router-link>
-        <h6>{{profile.public_repos}}</h6>
+  <div class="the-container">
+    <div class="the-flexy-container">
+      <div class="profileData-left">
+        <!-- <div v-for="profile in profiles" v-bind:key="profile.id">
         </div> -->
-      <div v-if="profile">
-        <p>Github_Name: {{ profile.name }}</p>
-        <p>Github_username: {{ profile.login }}</p>
-        <router-link to="/https://github.com/oyindamolaWbCode">Profile Link</router-link>
-        <p></p>
-        <p>Numbers_of_repos: {{ profile.public_repos }}</p>
+        <div v-if="profile">
+          <div class="image">
+            <img :src="Image" class="repo-profile"/>
+          </div>
+          <p>Github_Name: {{ profile.name }}</p>
+          <p>Github_username: {{ profile.login }}</p>
+          <router-link to="/https://github.com/oyindamolaWbCode">Profile Link</router-link>
+          <p>Numbers_of_repos: {{ profile.public_repos }}</p>
+          <p>Site_Admin: {{ profile.site_admin }}</p>
+        </div>
+        <div v-else>
+          Loading...
+        </div>
       </div>
-      <div v-else>
-        Loading...
-      </div>
+      <div class="reposList">
 
+      </div>
     </div>
-  </main>
+  </div>
 </template>
 
 <style >
+@import url('https://fonts.googleapis.com/css2?family=Commissioner:wght@200&family=Josefin+Sans:ital@1&family=Montserrat:wght@100;600&family=Overpass:wght@300&family=Poppins:wght@300&family=Roboto:wght@300&display=swap');
 
+ .the-container{
+  padding:20px;
+ }
+
+ .repo-profile{
+  background-size: cover;
+  background-repeat: no-repeat;
+  border-radius: 48%;
+  margin-bottom: 30px;
+  margin-top: 30px;
+  width: 250px;
+ }
+
+ .the-flexy-container{
+  display:flex;
+  justify-content: space-between;
+  width:100%;
+ }
+
+ .profileData-left{
+  width:30%;
+ }
+
+ p{
+  font-family: 'Montserrat', sans-serif;
+ }
 </style>
